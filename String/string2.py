e7142afe5fde40f9cd40bc6962890d7df0922bdf
Ajay <ajayidc007@gmail.com>
2020-02-14 16:53:04 +0530

    Update string2.py

--- String/string2.py
+++ String/string2.py
@@ -1,7 +1,5 @@
 # double_char
-
 # Given a string, return a string where for every char in the original, there are two chars.
-
 
 # double_char('The') → 'TThhee'
 # double_char('AAbb') → 'AAAAbbbb'
@@ -14,9 +12,10 @@ def double_char(str):
   
   return newstring
 
+######################################################################################################################################
+
 # count_hi
 # Return the number of times that the string "hi" appears anywhere in the given string.
-
 
 # count_hi('abc hi ho') → 1
 # count_hi('ABChi hi') → 2
@@ -31,10 +30,10 @@ def count_hi(str):
       count+=1
   return count
 
-#cat_dog
+######################################################################################################################################
 
+# cat_dog
 # Return True if the string "cat" and "dog" appear the same number of times in the given string.
-
 
 # cat_dog('catdog') → True
 # cat_dog('catcat') → False
@@ -53,11 +52,11 @@ def cat_dog(str):
   else:
     return False
 
-#count_code
+######################################################################################################################################
 
+# count_code
 # Return the number of times that the string "code" appears anywhere in the given string, except we'll accept 
 # any letter for the 'd', so "cope" and "cooe" count.
-
 
 # count_code('aaacodebbb') → 1
 # count_code('codexxcode') → 2
@@ -68,15 +67,14 @@ def count_code(str):
   for i in range(len(str)-3):
     if str[i]=='c' and str[i+1]=='o' and str[i+3]=='e':
       count+=1
-      
   return count
-  
- #end_other
 
+######################################################################################################################################
+
+# end_other
 # Given two strings, return True if either of the strings appears at the very end of the other string, 
 # ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). 
 # Note: s.lower() returns the lowercase version of a string.
-
 
 # end_other('Hiabc', 'abc') → True
 # end_other('AbC', 'HiaBc') → True
@@ -90,11 +88,12 @@ def end_other(a, b):
     return True
   else:
     return False
- 
-#xyz_there
+  
+######################################################################################################################################
+
+# xyz_there
 # Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.).
 # So "xxyz" counts but "x.xyz" does not.
-
 
 # xyz_there('abcxyz') → True
 # xyz_there('abc.xyz') → False
@@ -113,4 +112,4 @@ def xyz_there(str):
   return False
     
   
-  
+######################################################################################################################################  
